94db52d1f703c5f9242bb09555c3eb420505dc7f
wbcat <konokaze0@gmail.com>
2025-01-14 15:36:48 +0900

    directory: samplecomponent

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import "./pages/GeneratePage.tsx"
-import GeneratePages from './pages/GeneratePage.tsx'
-import APIRequestTest from './pages/APIRequestTest.tsx'
-import FileDownloadButton from './pages/FileDownloadButton.tsx'
+import "./SampleComponents/GeneratePage.tsx"
+import GeneratePages from './SampleComponents/GeneratePage.tsx'
+import APIRequestTest from './SampleComponents/APIRequestTest.tsx'
+import FileDownloadButton from './SampleComponents/FileDownloadButton.tsx'
 
 function App() {
 
