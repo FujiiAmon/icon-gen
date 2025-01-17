ad44f42e4944548b5a4d75841b38809aad5c5e50
FujiiAmon <amondealwis@gmail.com>
2025-01-17 21:17:22 +0900

    a

--- api/api.py
+++ api/api.py
@@ -1,11 +1,28 @@
+from openai import OpenAI
 import json
-
+import os 
+import openai
+import base64
+#a
 with open("src/api/api_key.json") as f:
     api_key = json.load(f)["API_KEY"]
-    print(api_key)
+
+# APIキーを設定
+os.environ["OPENAI_API_KEY"] = api_key
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+
 #引数が文字列
-def image(a):
-    return a
+def icon_create(youser_prompt):
+    client = OpenAI()
+    response = client.images.generate(
+    model="dall-e-3",
+    prompt=youser_prompt,
+    size="1024x1024",
+    quality="standard",
+    n=1
+    )
+    image_bytes = base64.b64decode(response.data[0].b64_json)#返り値をどうにかしたい
+    return image_bytes
 
-
-#返り値画像
+print(icon_create("りんご"))
